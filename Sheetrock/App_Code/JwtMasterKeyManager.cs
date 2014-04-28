d1d031518e9972d959ea2da13db3239908f6a737
Christopher Smith <cbsmith@cbsmith.org>
2014-04-28 13:18:27 +0000

    Part One
    Extended the Microsoft.AspNet.Identity.Samples boilerplate with a JWT
    token action

---
+++ Sheetrock/App_Code/JwtMasterKeyManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Sheetrock
+{
+	public static class JwtMasterKeyManager
+	{
+		public static byte[] GetEncodedMasterKey()
+		{
+			var sha256 = new SHA256Managed();
+			var secretBytes = System.Text.Encoding.UTF8.GetBytes(WebConfigurationManager.AppSettings["MasterKey"] + "JWTSig");
+			byte[] signingKey = sha256.ComputeHash(secretBytes);
+
+			return signingKey;
+		}
+	}
+}
